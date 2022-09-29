eeb3ed03132482fc8a268463202f40b95109f3e7
codky <msky0912@gmail.com>
2022-09-29 12:56:00 +0900

    Initial commits

---
+++ src/main/java/com/codky/book/springboot2webservice2/Springboot2Webservice2Application.java
@@ -0,0 +1,13 @@
+package com.codky.book.springboot2webservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2Webservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2Webservice2Application.class, args);
+    }
+
+}
