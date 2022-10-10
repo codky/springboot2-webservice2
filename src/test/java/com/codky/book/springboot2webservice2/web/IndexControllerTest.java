a193bb27736eaa27faa569fe7ad4ea8355792a2f
codky <msky0912@gmail.com>
2022-10-10 09:56:29 +0900

    mustache 기능 설정

---
+++ src/test/java/com/codky/book/springboot2webservice2/web/IndexControllerTest.java
@@ -0,0 +1,32 @@
+package com.codky.book.springboot2webservice2.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+
+        //then
+        assertThat(body).contains("Spring boot webservice");
+    }
+
+}
