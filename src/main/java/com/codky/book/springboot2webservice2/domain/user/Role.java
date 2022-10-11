4f7da7ccf15e395361a1c46b53cec6a96a195f61
codky <msky0912@gmail.com>
2022-10-11 12:55:33 +0900

    user entity

---
+++ src/main/java/com/codky/book/springboot2webservice2/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.codky.book.springboot2webservice2.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"), // 스프링 시큐리티에서는 권한 코드에 항상 ROLE_ 이 앞에 있어야 함.
+    USER("ROLE_USER", "일반사용자");
+
+    private final String key;
+    private final String title;
+}
