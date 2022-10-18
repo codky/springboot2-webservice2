ec4dff2fca8260b542c21e7c9f2d6c9e09971327
codky <msky0912@gmail.com>
2022-10-18 12:17:24 +0900

    oauth2 소셜로그인 설정(naver)

--- src/main/java/com/codky/book/springboot2webservice2/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/codky/book/springboot2webservice2/config/auth/dto/OAuthAttributes.java
@@ -26,7 +26,22 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 함
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+    // 구글 로그인을 등록하면서 대부분 코드가 확장성 있게 작성되었기 떄문에 네이버는 쉽게 등록 가능
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name(String.valueOf(response.get("name")))
+                .email(String.valueOf(response.get("email")))
+                .picture(String.valueOf(response.get("profile_image")))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
--- src/main/java/com/codky/book/springboot2webservice2/web/IndexController.java
+++ src/main/java/com/codky/book/springboot2webservice2/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.codky.book.springboot2webservice2.web;
 
+import com.codky.book.springboot2webservice2.config.auth.LoginUser;
 import com.codky.book.springboot2webservice2.config.auth.dto.SessionUser;
 import com.codky.book.springboot2webservice2.service.posts.PostsService;
 import com.codky.book.springboot2webservice2.web.dto.PostsResponseDto;
@@ -9,14 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
     @GetMapping("/")
